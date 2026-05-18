c94978a81ab9d48f94ece87a394baac3cbd0e141
Michael Gur <michael@thegur.net>
2026-05-18 09:41:18 +0300

    base fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-  base: "script-document-builder",
+  base: "/script-document-builder",
   staged: {
     "*": "vp check --fix",
   },
